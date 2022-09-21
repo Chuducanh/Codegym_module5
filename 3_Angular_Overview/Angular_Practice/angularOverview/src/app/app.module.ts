aaa3b58edeadde364f0114f5b17ecb9cb292d5df
Chuducanh <chanhducu@gmail.com>
2022-09-22 01:55:25 +0700

    Nop bai tap

--- 3_Angular_Overview/Angular_Practice/angularOverview/src/app/app.module.ts
+++ 3_Angular_Overview/Angular_Practice/angularOverview/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import { PetComponent } from './pet/pet.component';
+import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './product/product.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FontSizeEditorComponent,
+    PetComponent,
+    ProductComponent,
+    CalculatorComponent,
+    ColorPickerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
