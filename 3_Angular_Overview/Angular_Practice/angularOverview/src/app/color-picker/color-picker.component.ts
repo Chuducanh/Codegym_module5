8adcafd75b17320b26e96ce0e05b60b61e2ef9ae
Chuducanh <chanhducu@gmail.com>
2022-09-22 22:42:22 +0700

    Nop bai tap

--- 3_Angular_Overview/Angular_Practice/angularOverview/src/app/color-picker/color-picker.component.ts
+++ 3_Angular_Overview/Angular_Practice/angularOverview/src/app/color-picker/color-picker.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorPickerComponent implements OnInit {
 
+  colors = [
+    { id: 1, name: "#0000cc" },
+    { id: 2, name: "#cc0099" },
+    { id: 3, name: "#ffffcc" },
+    { id: 4, name: "#00cc66" },
+    { id: 5, name: "#663300" }
+  ];
+
+  code: string = this.colors[0].name;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeColorValue(code) {
+    this.code = code;
   }
 
 }
